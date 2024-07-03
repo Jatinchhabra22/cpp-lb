3197d1e13c003d4e90d4ba489a8974909215b2f4
Jatinchhabra22 <147986920+Jatinchhabra22@users.noreply.github.com>
2024-07-04 00:11:27 +0530

    19

--- code19.cpp
+++ code19.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int printArray(int arr[], int size){
 
-    cout<<" printing the array "<<endl;
+    cout<<" printing the array "<<" ";
     for(int i = 0; i<size; i++){
-        cout<<arr[i]<<endl;
+        cout<<arr[i]<<" ";
     }
     cout<<"done with the array "<<endl;
 
@@ -14,7 +14,7 @@ int printArray(int arr[], int size){
 int main(){
     int third[4] = {2,3,4,5};
     printArray(third,4);
-    
+    cout<<endl;
     int fourth[10] = {1,1};
     printArray(fourth,20);
 
